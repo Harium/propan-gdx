2e6aa81e0348679f9a401f1ca666618d033ff2bf
Yuri Pourre <yuripourre@gmail.com>
2017-11-13 18:29:13 -0200

    Fix propan init

--- src/main/java/com/harium/propan/Propan.java
+++ src/main/java/com/harium/propan/Propan.java
@@ -1,12 +1,15 @@
 package com.harium.propan;
 
 import com.harium.etyl.DesktopEngine;
+import com.harium.etyl.core.Core;
 import com.harium.etyl.core.Engine;
 import com.harium.etyl.loader.FontLoader;
 import com.harium.etyl.loader.MultimediaLoader;
 import com.harium.etyl.loader.image.ImageLoader;
 import com.harium.etyl.util.PathHelper;
+import com.harium.propan.core.CoreGL;
 import com.harium.propan.core.context.ApplicationGL;
+import com.harium.propan.core.loader.MeshLoader;
 
 public abstract class Propan extends DesktopEngine implements Engine<ApplicationGL> {
 
@@ -20,9 +23,14 @@ public abstract class Propan extends DesktopEngine implements Engine<Application
         initialSetup("");
 
         application = startApplication();
+        application.setLoaded(false);
         core.setApplication(application);
 
         super.init();
+    }
+
+    protected Core initCore() {
+        return new CoreGL(w, h);
     }
 
     protected void initialSetup(String suffix) {
@@ -31,6 +39,7 @@ public abstract class Propan extends DesktopEngine implements Engine<Application
         ImageLoader.getInstance().setPath(path);
         FontLoader.getInstance().setPath(path);
         MultimediaLoader.getInstance().setPath(path);
+        MeshLoader.getInstance().setPath(path);
     }
 
 }
